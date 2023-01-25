1bf7182dec01a274065b48223fb5cc91ab36ba53
user <dimochka1994@yandex.ru>
2023-01-25 10:12:44 +0300

    Поменял VerifyCSRFMiddleware для модуля интеграции

--- app/Services/Traveline/Infrastructure/Providers/BootServiceProvider.php
+++ app/Services/Traveline/Infrastructure/Providers/BootServiceProvider.php
@@ -2,13 +2,15 @@
 
 namespace GTS\Services\Traveline\Infrastructure\Providers;
 
+use GTS\Services\Traveline\Interface\Site\Http\Middleware\VerifyCsrfToken;
+use GTS\Shared\Interface\Common\Http\Middleware\VerifyCsrfToken as SharedVerifyCsrfToken;
 use Illuminate\Support\ServiceProvider;
 
 
 class BootServiceProvider extends ServiceProvider {
 
     public function register() {
-
+        $this->app->bind(SharedVerifyCsrfToken::class, VerifyCsrfToken::class);
     }
 
     public function boot() {
