cdc219aeec83283bdf63eeaf672a50dd6a461b43
s16121986 <s16121986@yandex.ru>
2023-01-30 10:30:23 +0300

    feat: decorators

---
+++ app/Shared/UI/Common/Helpers/Phone/Format/RU.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GTS\Shared\UI\Common\Helpers\Phone\Format;
+
+class RU extends DefaultFormat
+{
+    protected string $countryCode = '7';
+}
---
+++ app/Shared/UI/Common/Helpers/Phone/Format/UZ.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GTS\Shared\UI\Common\Helpers\Phone\Format;
+
+class UZ extends DefaultFormat
+{
+    protected string $countryCode = '998';
+}
