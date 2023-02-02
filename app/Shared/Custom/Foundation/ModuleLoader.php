5032a80bd5955de4cee2c31e2cabf26a430252fa
s16121986 <s16121986@yandex.ru>
2023-02-02 12:28:12 +0300

    feat: portGateway

--- app/Shared/Custom/Foundation/ModuleLoader.php
+++ app/Shared/Custom/Foundation/ModuleLoader.php
@@ -6,21 +6,31 @@ class ModuleLoader
 {
     public function __construct(private readonly Application $app) {}
 
-    public function load($abstract): bool
+    public function loadByAbstract($abstract): bool
     {
-        if (!str_ends_with($abstract, 'FacadeInterface'))
+        $module = $this->findInterface($abstract, 'Facade')
+            ?? $this->findInterface($abstract, 'Port');
+        if (!$module)
             return false;
+
+        $this->app->modules()->loadModule($module);
+
+        return true;
+    }
+
+    private function findInterface($abstract, $prefix)
+    {
+        if (!str_ends_with($abstract, $prefix . 'Interface'))
+            return null;
         //!str_starts_with($abstract, 'GTS\\')
         //|| str_starts_with($abstract, 'GTS\\Shared')
 
         foreach ($this->app->modules()->registeredModules() as $module) {
-            if (str_starts_with($abstract, $module->namespace('\\Infrastructure\\Facade\\'))) {
-                $this->app->loadModule($module->name());
-                //var_dump($module->name());
-                return true;
+            if (str_starts_with($abstract, $module->namespace('\\Infrastructure\\' . $prefix . '\\'))) {
+                return $module;
             }
         }
 
-        return false;
+        return null;
     }
 }
