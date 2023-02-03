548860933adb14eba62da76d2465b9a00a01abc8
s16121986 <s16121986@yandex.ru>
2023-02-03 11:33:44 +0300

    fix: Shared model

--- app/Shared/Infrastructure/Models/Model.php
+++ app/Shared/Infrastructure/Models/Model.php
@@ -4,5 +4,21 @@ namespace GTS\Shared\Infrastructure\Models;
 
 class Model extends \Illuminate\Database\Eloquent\Model
 {
+    public function setAttribute($key, $value)
+    {
+        //dd($this->isClassCastable($key));
+//        $this->validateAttribute($key, $value);
 
+        if (method_exists($this, 'setTranslatableAttribute') && $this->isTranslatable($key))
+            $this->setTranslatableAttribute($key, $value);
+        else
+            parent::setAttribute($key, $value);
+
+        return $this;
+    }
+
+//    public function getCasts() {
+//        //dd(array_merge($this->getValidationCastTypes(), parent::getCasts()));
+//        return array_merge($this->getValidationCastTypes(), parent::getCasts());
+//    }
 }
