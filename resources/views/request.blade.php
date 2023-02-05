544c3cdd6aa9d4f6cf7bffbff450a9c1eb47b95e
user <dimochka1994@yandex.ru>
2023-02-05 12:35:03 +0300

    feat: build manifest and port requests commands

---
+++ resources/views/request.blade.php
@@ -0,0 +1,32 @@
+<?= '<?php' ?>
+
+namespace GTS\Shared\Domain\Adapter\Request\{{$module}}\{{$port}};
+
+class {{\Str::ucfirst($method)}}Request implements \GTS\Shared\Domain\Adapter\RequestInterface {
+
+    public function __construct(
+    @foreach($arguments as $index => $argument)
+        public readonly {{$argument->isNullable ? '?' : ''}}\{{$argument->type}} ${{$argument->name}} @if($argument->isDefaultValueAvailable ? " = {$argument->defaultValue}" : '') @endif,
+    @endforeach
+{!! ')' !!} {}
+
+    public function module(): string {
+        return '{{$module}}';
+    }
+
+    public function port(): string {
+        return '{{$port}}';
+    }
+
+    public function method(): string {
+        return '{{$method}}';
+    }
+
+    public function arguments(): array {
+        return [
+        @foreach($arguments as $argument)
+            '{{$argument->name}}' => $this->{{$argument->name}},
+        @endforeach
+        ];
+    }
+}
