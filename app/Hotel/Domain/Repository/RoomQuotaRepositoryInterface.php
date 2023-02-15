136b5ce2f7757b052b139e60f658a2e978fd2655
user <dimochka1994@yandex.ru>
2023-02-15 13:22:15 +0300

    feat: hotel open/close quota commands

--- app/Hotel/Domain/Repository/RoomQuotaRepositoryInterface.php
+++ app/Hotel/Domain/Repository/RoomQuotaRepositoryInterface.php
@@ -7,4 +7,8 @@ use Carbon\CarbonPeriod;
 interface RoomQuotaRepositoryInterface
 {
     public function updateRoomQuota(int $roomId, CarbonPeriod $period, int $quota);
+
+    public function closeRoomQuota(int $roomId, CarbonPeriod $period);
+
+    public function openRoomQuota(int $roomId, CarbonPeriod $period);
 }
--- app/Hotel/Infrastructure/Repository/RoomQuotaRepository.php
+++ app/Hotel/Infrastructure/Repository/RoomQuotaRepository.php
@@ -6,6 +6,7 @@ use Carbon\CarbonPeriod;
 
 use GTS\Hotel\Domain\Repository\RoomQuotaRepositoryInterface;
 use GTS\Hotel\Infrastructure\Models\Room\Quota as EloquentQuota;
+use GTS\Hotel\Infrastructure\Models\Room\QuotaTypeEnum;
 
 class RoomQuotaRepository implements RoomQuotaRepositoryInterface
 {
@@ -15,5 +16,21 @@ class RoomQuotaRepository implements RoomQuotaRepositoryInterface
             ->where('date', '>=', $period->getStartDate())
             ->where('date', '<', $period->getEndDate())
             ->update(['count_available' => $quota]);
+    }
+
+    public function closeRoomQuota(int $roomId, CarbonPeriod $period)
+    {
+        EloquentQuota::whereRoomId($roomId)
+            ->where('date', '>=', $period->getStartDate())
+            ->where('date', '<', $period->getEndDate())
+            ->update(['type' => QuotaTypeEnum::CLOSE]);
+    }
+
+    public function openRoomQuota(int $roomId, CarbonPeriod $period)
+    {
+        EloquentQuota::whereRoomId($roomId)
+            ->where('date', '>=', $period->getStartDate())
+            ->where('date', '<', $period->getEndDate())
+            ->update(['type' => QuotaTypeEnum::OPEN]);
     }
 }
