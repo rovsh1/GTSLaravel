54f5a1c4aab2440e1845e77c985d4c44aba98d34
s16121986 <s16121986@yandex.ru>
2023-02-22 00:36:12 +0300

    feat: console

--- apps/core/helpers.php
+++ apps/core/helpers.php
@@ -7,6 +7,31 @@ function module(string $name): ?Module
     return app('modules')->get($name);
 }
 
+function root_path($path = '')
+{
+    return app()->rootPath($path);
+}
+
+function core_path($path = ''): string
+{
+    return app()->corePath($path);
+}
+
+function admin_path($path = ''): string
+{
+    return app('path.admin') . ($path ? DIRECTORY_SEPARATOR . $path : '');
+}
+
+function site_path($path = ''): string
+{
+    return app('path.site') . ($path ? DIRECTORY_SEPARATOR . $path : '');
+}
+
+function api_path($path = ''): string
+{
+    return app('path.api') . ($path ? DIRECTORY_SEPARATOR . $path : '');
+}
+
 function modules_path($path = '')
 {
     return app()->modulesPath($path);
