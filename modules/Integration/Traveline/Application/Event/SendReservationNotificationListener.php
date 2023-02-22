248746130b96cc6a9c6bd0b6dd718935d10a2775
user <dimochka1994@yandex.ru>
2023-02-22 09:44:28 +0300

    refactor: traveline

--- modules/Integration/Traveline/Application/Event/SendReservationNotificationListener.php
+++ modules/Integration/Traveline/Application/Event/SendReservationNotificationListener.php
@@ -4,11 +4,11 @@ namespace Module\Integration\Traveline\Application\Event;
 
 use Custom\Framework\Contracts\Event\DomainEventInterface;
 use Custom\Framework\Contracts\Event\DomainEventListenerInterface;
-use Module\Services\Traveline\Infrastructure\Adapter\Traveline\AdapterInterface;
+use Module\Integration\Traveline\Domain\Adapter\TravelineAdapterInterface;
 
 class SendReservationNotificationListener implements DomainEventListenerInterface
 {
-    public function __construct(private AdapterInterface $adapter) {}
+    public function __construct(private TravelineAdapterInterface $adapter) {}
 
     public function handle(DomainEventInterface $event)
     {
