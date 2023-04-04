a86b7d3d83f0b0d11b5ec45c89eb1ee61c68d9eb
s16121986 <s16121986@yandex.ru>
2023-04-04 17:57:13 +0300

    fix: migrations

--- database/migrations/hotel/2023_03_21_081202_create_hotel_rooms_table.php
+++ database/migrations/hotel/2023_03_21_081202_create_hotel_rooms_table.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('hotel_rooms', function (Blueprint $table) {
