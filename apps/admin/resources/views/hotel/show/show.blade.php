01bb7ef1ac563fdf959059318d622a87c6caf77f
Гурьянов Сергей <s16121986@yandex.ru>
2023-04-21 19:52:04 +0300

    fix: ui

--- apps/admin/resources/views/hotel/show/show.blade.php
+++ apps/admin/resources/views/hotel/show/show.blade.php
@@ -58,7 +58,7 @@
         </div>
 
         <div class="mt-3">
-            <x-ui.card :collapsable="true" header="Объекты и достопримечательности ({{$model->landmarks->count()}})">
+            <x-ui.card class="card card-grid" :collapsable="true" header="Объекты и достопримечательности ({{$model->landmarks->count()}})">
                 @if($notesUrl)
                     <x-slot:header-controls>
                         <button id="btn-hotel-landmarks" class="btn btn-add" data-url="{{$landmarkUrl}}">
