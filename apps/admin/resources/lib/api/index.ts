79ce8e2db3ce032dc00e09b8273e54db237ae39d
Daniil Pronin <prn@feip.co>
2023-04-26 20:48:11 +0700

    code style

--- apps/admin/resources/lib/api/index.ts
+++ apps/admin/resources/lib/api/index.ts
@@ -1,20 +1,18 @@
 import { createFetch } from '@vueuse/core'
-import * as qs from 'qs'
+import qs from 'qs'
 
 import { ADMIN_API_URL } from '~resources/lib/env'
 
-const useAdminAPI = createFetch({
+export const useAdminAPI = createFetch({
   baseUrl: ADMIN_API_URL,
   options: {
     immediate: false,
   },
 })
 
-const getURL = (url: string, query?: Record<string, string | number>): string => {
+export const getURL = (url: string, query?: Record<string, string | number>): string => {
   if (query === undefined) {
     return url
   }
   return `${url}?${qs.stringify(query)}`
 }
-
-export { useAdminAPI, getURL }
