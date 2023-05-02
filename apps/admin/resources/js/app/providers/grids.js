fc386d3628db8c8a508140520d6c1e0b92b4add6
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-05-02 13:27:28 +0300

    refactor: try to fix daterangepicker click outside bug

--- apps/admin/resources/js/app/providers/grids.js
+++ apps/admin/resources/js/app/providers/grids.js
@@ -2,21 +2,26 @@ export default function bootGrid() {
   const $btn = $('#btn-grid-filters')
   const $popup = $('#grid-filters-popup')
   const close = (e) => {
-    const isClickByDateRangeCalendar = $('.daterangepicker').find(e.target).length > 0
-    if (!$popup.is(e.target) && $popup.find(e.target).length === 0 && !isClickByDateRangeCalendar) {
+    const dateRangeChildren = document.querySelectorAll('.daterangepicker *')
+    let isClickedByDateRangeChildren = false
+    if (dateRangeChildren !== undefined) {
+      isClickedByDateRangeChildren = [...dateRangeChildren].find((element) => element === e.target)
+    }
+
+    if (!$popup.is(e.target) && $popup.find(e.target).length === 0 && !isClickedByDateRangeChildren) {
       $popup.hide()
-      $(document).unbind('click', close)
+      $(document).off('click', close)
     }
   }
 
-  $btn.click((e) => {
+  $btn.on('click', (e) => {
     e.preventDefault()
     if (!$popup.is(':hidden')) {
       return
     }
 
     $popup.fadeIn(200)
-    $(document).click(close)
+    $(document).on('click', close)
     e.stopPropagation()
   })
 
--- apps/admin/resources/js/vendor/daterangepicker.js
+++ apps/admin/resources/js/vendor/daterangepicker.js
@@ -7,24 +7,12 @@ const defaultSettings = {
     // eslint-disable-next-line quote-props
     'Сегодня': [moment(), moment()],
     // eslint-disable-next-line quote-props
-    'Вчера': [moment()
-      .subtract(1, 'days'), moment()
-      .subtract(1, 'days')],
-    'Последние 7 дней': [moment()
-      .subtract(6, 'days'), moment()],
-    'Последние 30 дней': [moment()
-      .subtract(29, 'days'), moment()],
-    'Этот месяц': [moment()
-      .startOf('month'), moment()
-      .endOf('month')],
-    'Прошлый месяц': [moment()
-      .subtract(1, 'month')
-      .startOf('month'), moment()
-      .subtract(1, 'month')
-      .endOf('month')],
-    'Текущий год': [moment()
-      .startOf('year'), moment()
-      .endOf('year')],
+    'Вчера': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+    'Последние 7 дней': [moment().subtract(6, 'days'), moment()],
+    'Последние 30 дней': [moment().subtract(29, 'days'), moment()],
+    'Этот месяц': [moment().startOf('month'), moment().endOf('month')],
+    'Прошлый месяц': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')],
+    'Текущий год': [moment().startOf('year'), moment().endOf('year')],
   },
   alwaysShowCalendars: true,
   autoUpdateInput: false,
