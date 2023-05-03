a9813d6e33cf889322f57981c78ecef33688aa72
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-05-03 12:20:40 +0300

    fix: create hotels table text field instead json

--- database/migrations/deprecated/2023_03_14_000001_fill_hotels.php
+++ database/migrations/deprecated/2023_03_14_000001_fill_hotels.php
@@ -4,24 +4,53 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
+
+    private const TOUR_FEE = 12;
+    private const VAT = 13;
+
+    private const CONDITION_CHECK_IN = 1;
+    private const CONDITION_CHECK_OUT = 2;
+
     public function up()
     {
         $q = DB::connection('mysql_old')->table('hotels');
         foreach ($q->cursor() as $r) {
-            $conditions = DB::connection('mysql_old')
-                ->table('hotel_residence_conditions')
-                ->where('hotel_id', $r->id)
-                ->get();
-
-            $conditionsJson = null;
-            if ($conditions->count() > 0) {
-                $conditionsJson = $conditions->map(fn(\stdClass $condition) => [
-                    'type' => $condition->type,
-                    'price_markup' => $condition->price_markup,
-                    'start_time' => $condition->start,
-                    'end_time' => $condition->end,
-                ])->toJson();
-            }
+//            $conditionsIndexedByType = DB::connection('mysql_old')
+//                ->table('hotel_residence_conditions')
+//                ->where('hotel_id', $r->id)
+//                ->groupBy('type')
+//                ->get();
+//
+//            //@todo что делать с room_id?
+//            $margins = DB::connection('mysql_old')
+//                ->table('hotel_margins')
+//                ->where('hotel_id', $r->id)
+//                ->get();
+//
+//            $options = DB::connection('mysql_old')
+//                ->table('hotel_options')
+//                ->where('hotel_id', $r->id)
+//                ->whereIn('option', [self::TOUR_FEE, self::VAT])
+//                ->get();
+//
+//            $cancelPeriods = DB::connection('mysql_old')
+//                ->table('hotel_cancel_period')
+//                ->where('hotel_id', $r->id)
+//                ->get();
+//
+//            $cancelConditions = collect();
+//            foreach ($cancelPeriods as $cancelPeriod) {
+//                $cancelConditions = DB::connection('mysql_old')
+//                    ->table('hotel_cancel_conditions')
+//                    ->where('period_id', $cancelPeriod->id)
+//                    ->get();
+//            }
+//
+//
+//            $markupSettingsJson = null;
+//            if ($conditionsIndexedByType->count() > 0) {
+//
+//            }
 
             DB::table('hotels')
                 ->insert([
@@ -37,7 +66,7 @@ return new class extends Migration {
                     'zipcode' => $r->zipcode,
                     'status' => $r->status,
                     'visibility' => $this->getVisibilityValue($r->visible_for),
-                    'additional_conditions' => $conditionsJson,
+                    'markup_settings' => null,
                     'created_at' => $r->created,
                     'updated_at' => $r->updated,
                 ]);
