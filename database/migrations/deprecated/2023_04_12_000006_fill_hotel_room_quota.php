b73f81a04f25d5f988b562357204869c32963247
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-05-03 12:15:11 +0300

    feat: fill hotel room quotas

---
+++ database/migrations/deprecated/2023_04_12_000006_fill_hotel_room_quota.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        $q = DB::connection('mysql_old')->table('hotel_room_quotes');
+        foreach ($q->cursor() as $r) {
+            DB::table('hotel_room_quota')
+                ->insert([
+                    'room_id' => $r->room_id,
+                    'date' => $r->date,
+                    'release_days' => $r->period,
+                    'count_available' => $r->count_available,
+                    'count_booked' => $r->count_booked,
+                    //@todo count_reserved нужно смотреть как считается
+                    'count_reserved' => 0,
+                    'status' => $r->type,
+                ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::table('hotel_room_quota')->truncate();
+    }
+};
