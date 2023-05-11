173646cfd81cf8635bc90bacae10da9290e4f538
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-05-11 12:48:40 +0300

    fix: negative count_available fix

--- database/migrations/deprecated/2023_04_12_000006_fill_hotel_room_quota.php
+++ database/migrations/deprecated/2023_04_12_000006_fill_hotel_room_quota.php
@@ -10,13 +10,18 @@ return new class extends Migration {
     {
         $q = DB::connection('mysql_old')->table('hotel_room_quotes');
         foreach ($q->cursor() as $r) {
+            $countAvailable = $r->count_available - $r->count_booked;
+            if ($countAvailable < 0) {
+                //@hack на проде есть данные где отрицательные available
+                $countAvailable = 0;
+            }
             DB::table('hotel_room_quota')
                 ->insert([
                     'room_id' => $r->room_id,
                     'date' => $r->date,
                     'release_days' => $r->period,
                     'count_total' => $r->count_available,
-                    'count_available' => $r->count_available - $r->count_booked,
+                    'count_available' => $countAvailable,
                     'count_booked' => $r->count_booked,
                     //@todo count_reserved нужно смотреть как считается
                     'count_reserved' => 0,
