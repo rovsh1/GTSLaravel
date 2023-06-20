521d06d0d84d830d747ea4aff6e3869257e80697
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-06-20 13:14:41 +0300

    feat: hotel prices

--- apps/admin/resources/views/hotel/prices/prices.ts
+++ apps/admin/resources/views/hotel/prices/prices.ts
@@ -1,5 +1,10 @@
+import { createVueInstance } from '~lib/vue'
+
+import HotelPrices from './HotelPrices.vue'
+
 import '~resources/views/main'
 
-$(() => {
-
+createVueInstance({
+  rootComponent: HotelPrices,
+  rootContainer: '#hotel-prices',
 })
