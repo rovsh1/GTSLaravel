d454d7b67057b9fa724d894a448dbf015e78727a
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-06-28 17:47:04 +0300

    refactor: CurrencyEnum and id matching

--- modules/Shared/Enum/CurrencyEnum.php
+++ modules/Shared/Enum/CurrencyEnum.php
@@ -10,19 +10,19 @@ enum CurrencyEnum: string
     private const KZT_ID = 4;
     private const EUR_ID = 10;
 
-    case BYN = 'BYN';
+//    case BYN = 'BYN';
     case EUR = 'EUR';
     case RUB = 'RUB';
     case USD = 'USD';
     case KZT = 'KZT';
-    case CHF = 'CHF';
-    case SEK = 'SEK';
-    case UAH = 'UAH';
-    case GBP = 'GBP';
-    case CZK = 'CZK';
-    case NOK = 'NOK';
-    case DKK = 'DKK';
-    case JPY = 'JPY';
+//    case CHF = 'CHF';
+//    case SEK = 'SEK';
+//    case UAH = 'UAH';
+//    case GBP = 'GBP';
+//    case CZK = 'CZK';
+//    case NOK = 'NOK';
+//    case DKK = 'DKK';
+//    case JPY = 'JPY';
     case UZS = 'UZS';
 
     public function id(): int
