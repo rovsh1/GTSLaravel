81e14a22a8a65538679734f0317e8411ba384d08
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-08-08 18:09:55 +0300

    feat: send traveline notifications every minute, if not accepted booking exists (GTS-1984)

--- modules/Integration/Traveline/Infrastructure/Jobs/Legacy/SendTravelineNotifications.php
+++ modules/Integration/Traveline/Infrastructure/Jobs/Legacy/SendTravelineNotifications.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Module\Integration\Traveline\Infrastructure\Jobs\Legacy;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\ClientInterface;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
