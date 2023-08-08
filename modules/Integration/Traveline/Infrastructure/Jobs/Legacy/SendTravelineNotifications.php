2f3943724b8f628ee695e1e66001127d54795747
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-08-08 18:07:01 +0300

    feat: send GoToStans as customer to traveline

--- modules/Integration/Traveline/Infrastructure/Jobs/Legacy/SendTravelineNotifications.php
+++ modules/Integration/Traveline/Infrastructure/Jobs/Legacy/SendTravelineNotifications.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace Module\Integration\Traveline\Infrastructure\Jobs\Legacy;
 
+use GuzzleHttp\ClientInterface;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Module\Integration\Traveline\Domain\Adapter\TravelineAdapterInterface;
 use Module\Integration\Traveline\Infrastructure\Adapter\TravelineAdapter;
 use Module\Integration\Traveline\Infrastructure\Models\Legacy\TravelineReservation;
 
@@ -16,14 +18,21 @@ class SendTravelineNotifications implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function handle(TravelineAdapter $travelineAdapter): void
+    public function handle(): void
     {
         \Log::debug('[SendTravelineNotifications] Start');
         if (!TravelineReservation::whereNull('accepted_at')->exists()) {
             return;
         }
+        $travelineAdapter = $this->getTravelineAdapter();
         $travelineAdapter->sendReservationNotification();
         \Log::debug('[SendTravelineNotifications] End');
     }
 
+    private function getTravelineAdapter(): TravelineAdapterInterface
+    {
+        $notificationsUrl = config('modules.Traveline.notifications_url');
+
+        return new TravelineAdapter(app(ClientInterface::class), $notificationsUrl);
+    }
 }
