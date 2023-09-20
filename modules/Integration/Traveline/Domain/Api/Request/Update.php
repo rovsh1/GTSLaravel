1443816f59e9cf28f8b40bc98cb14a6bef767728
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-09-20 20:48:53 +0300

    fix: open quota

--- modules/Integration/Traveline/Domain/Api/Request/Update.php
+++ modules/Integration/Traveline/Domain/Api/Request/Update.php
@@ -50,7 +50,7 @@ class Update
 
     public function isOpened(): bool
     {
-        return !$this->isClosed() && $this->ratePlanId !== null;
+        return !$this->isClosed() && $this->ratePlanId !== null && $this->quota !== null;
     }
 
     public static function fromArray(array $data, HotelRoomCodeGeneratorInterface $codeGenerator): self
