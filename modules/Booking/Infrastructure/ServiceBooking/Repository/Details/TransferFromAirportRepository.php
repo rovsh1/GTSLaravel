d97f321d8d189732960c52d5efec3eba92dab25f
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-10-17 18:48:20 +0300

    fix: airportId details storing

--- modules/Booking/Infrastructure/ServiceBooking/Repository/Details/TransferFromAirportRepository.php
+++ modules/Booking/Infrastructure/ServiceBooking/Repository/Details/TransferFromAirportRepository.php
@@ -56,7 +56,7 @@ class TransferFromAirportRepository extends AbstractDetailsRepository implements
         return (bool)Transfer::whereId($details->id()->value())->update([
             'data' => [
                 'serviceInfo' => $this->serializeServiceInfo($details->serviceInfo()),
-                'airportId' => $details->airportId(),
+                'airportId' => $details->airportId()->value(),
                 'flightNumber' => $details->flightNumber(),
                 'meetingTablet' => $details->meetingTablet(),
                 'carBids' => $details->carBids()->toData(),
