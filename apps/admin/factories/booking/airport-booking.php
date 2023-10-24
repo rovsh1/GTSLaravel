7da01f95ceba89da8983c5b514683cd73ea2d80f
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-10-24 14:10:41 +0300

    feat: hide deprecated booking factories

--- apps/admin/factories/booking/airport-booking.php
+++ apps/admin/factories/booking/airport-booking.php
@@ -2,18 +2,18 @@
 
 use App\Admin\Components\Factory\Factory;
 
-return Factory::key('airport-booking')
-    ->category(Factory::CATEGORY_BOOKING)
-    ->group('bookings')
-    //   ->model(\App\Admin\Models\System\Constant::class)
-    ->controller(\App\Admin\Http\Controllers\Booking\Airport\BookingController::class)
-    ->titles([
-        "index" => "Брони услуг аэропорта",
-        "create" => "Новая бронь"
-    ])
-    ->views([
-        'index' => 'airport-booking.main.main',
-        'show' => 'airport-booking.show.show',
-        'form' => 'airport-booking.form.form'
-    ])
-    ->priority(208);
+//return Factory::key('airport-booking')
+//    ->category(Factory::CATEGORY_BOOKING)
+//    ->group('bookings')
+//    //   ->model(\App\Admin\Models\System\Constant::class)
+//    ->controller(\App\Admin\Http\Controllers\Booking\Airport\BookingController::class)
+//    ->titles([
+//        "index" => "Брони услуг аэропорта",
+//        "create" => "Новая бронь"
+//    ])
+//    ->views([
+//        'index' => 'airport-booking.main.main',
+//        'show' => 'airport-booking.show.show',
+//        'form' => 'airport-booking.form.form'
+//    ])
+//    ->priority(208);
