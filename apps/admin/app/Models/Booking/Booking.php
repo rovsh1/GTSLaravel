bfa430bc106a4c2c1f7460d6ac8a77fdf71a219a
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-10-31 18:18:37 +0300

    fix: hotel booking page filters (GTS-2173)

--- apps/admin/app/Models/Booking/Booking.php
+++ apps/admin/app/Models/Booking/Booking.php
@@ -5,15 +5,21 @@ declare(strict_types=1);
 namespace App\Admin\Models\Booking;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as Query;
 
 class Booking extends \Module\Booking\Infrastructure\ServiceBooking\Models\Booking
 {
     public function scopeWithoutHotelBooking(Builder $builder): void
     {
-        $builder->whereNotExists(function (\Illuminate\Database\Query\Builder $query) {
+        $builder->whereNotExists(function (Query $query) {
             $query->selectRaw(1)
                 ->from('booking_hotel_details')
                 ->whereColumn($this->table . '.id', '=', 'booking_hotel_details.booking_id');
         });
+    }
+
+    public function scopeWhereCityId(Builder $builder, int $cityId): void
+    {
+        $builder->where('hotels.city_id', $cityId);
     }
 }
