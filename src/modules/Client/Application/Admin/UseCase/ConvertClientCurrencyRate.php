7900990e38cc81e36d446da4e991717cb764e94e
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-11-03 13:54:17 +0300

    feat: client currency rate (GTS-2162)

---
+++ src/modules/Client/Application/Admin/UseCase/ConvertClientCurrencyRate.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Module\Client\Application\Admin\UseCase;
+
+use Sdk\Module\Contracts\UseCase\UseCaseInterface;
+
+class ConvertClientCurrencyRate implements UseCaseInterface
+{
+
+}
---
+++ src/modules/Client/Domain/Repository/CurrencyRateRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Module\Client\Domain\Repository;
+
+interface CurrencyRateRepositoryInterface
+{
+
+}
