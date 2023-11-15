c3bcb0305e840be470c01c3d1b6d4802b1508967
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-11-15 12:37:11 +0300

    feat: client invoices (GTS-1795)

--- apps/admin/app/Http/Controllers/DashboardController.php
+++ apps/admin/app/Http/Controllers/DashboardController.php
@@ -9,6 +9,6 @@ class DashboardController extends Controller
     public function index()
     {
         //Sitemap::
-        return redirect(route('countries.index'));
+        return redirect(route('hotel-booking.index'));
     }
 }
