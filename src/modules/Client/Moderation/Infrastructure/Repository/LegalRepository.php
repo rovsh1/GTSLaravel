5ba71c438e5b3e6ab529a052d9524b9eb4e590f8
Гурьянов Сергей <s16121986@yandex.ru>
2023-11-25 00:21:19 +0300

    refactor: remove serializer

--- src/modules/Client/Moderation/Infrastructure/Repository/LegalRepository.php
+++ src/modules/Client/Moderation/Infrastructure/Repository/LegalRepository.php
@@ -8,13 +8,11 @@ use Module\Client\Moderation\Domain\Entity\Legal;
 use Module\Client\Moderation\Domain\Factory\LegalFactory;
 use Module\Client\Moderation\Domain\Repository\LegalRepositoryInterface;
 use Module\Client\Moderation\Infrastructure\Models\Legal as Model;
-use Module\Shared\Contracts\Service\SerializerInterface;
 
 class LegalRepository implements LegalRepositoryInterface
 {
     public function __construct(
-        private readonly LegalFactory $factory,
-        private readonly SerializerInterface $serializer
+        private readonly LegalFactory $factory
     ) {}
 
     public function get(int $id): ?Legal
@@ -32,7 +30,7 @@ class LegalRepository implements LegalRepositoryInterface
         return (bool)Model::whereId($legal->id()->value())->update([
             'name' => $legal->name(),
             'requisites' => $legal->requisites() !== null
-                ? $this->serializer->serialize($legal->requisites())
+                ? json_encode($legal->requisites())
                 : null,
         ]);
     }
--- src/modules/Hotel/Moderation/Domain/Hotel/Factory/HotelFactory.php
+++ src/modules/Hotel/Moderation/Domain/Hotel/Factory/HotelFactory.php
@@ -8,7 +8,6 @@ use Module\Hotel\Moderation\Domain\Hotel\ValueObject\Contact;
 use Module\Hotel\Moderation\Domain\Hotel\ValueObject\ContactCollection;
 use Module\Hotel\Moderation\Domain\Hotel\ValueObject\HotelId;
 use Module\Hotel\Moderation\Domain\Hotel\ValueObject\TimeSettings;
-use Module\Shared\Contracts\Service\SerializerInterface;
 use Module\Shared\Enum\ContactTypeEnum;
 use Module\Shared\Enum\CurrencyEnum;
 use Sdk\Module\Foundation\Support\EntityFactory\AbstractEntityFactory;
@@ -16,11 +15,6 @@ use Sdk\Module\Foundation\Support\EntityFactory\AbstractEntityFactory;
 class HotelFactory extends AbstractEntityFactory
 {
     protected string $entity = Hotel::class;
-
-    public function __construct(private readonly SerializerInterface $serializer)
-    {
-        parent::__construct();
-    }
 
     protected function fromArray(array $data): mixed
     {
@@ -30,7 +24,7 @@ class HotelFactory extends AbstractEntityFactory
             id: new HotelId($data['id']),
             name: $data['name'],
             currency: CurrencyEnum::from($data['currency']),
-            timeSettings: $this->serializer->deserialize(TimeSettings::class, $data['time_settings']),
+            timeSettings: TimeSettings::fromData(json_decode($data['time_settings'], true)),
             address: new Address(
                 country: $data['country_name'],
                 city: $data['city_name'],
