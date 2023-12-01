8630a44a02af0b29aeb00377705c6e3bc041b8b2
Гурьянов Сергей <s16121986@yandex.ru>
2023-12-01 11:40:29 +0300

    fix: booking events

--- src/modules/Booking/Requesting/Application/UseCase/SendRequest.php
+++ src/modules/Booking/Requesting/Application/UseCase/SendRequest.php
@@ -6,25 +6,28 @@ namespace Module\Booking\Requesting\Application\UseCase;
 
 use Module\Booking\Requesting\Domain\Booking\Service\RequestingRules;
 use Module\Booking\Requesting\Domain\BookingRequest\Factory\RequestFactory;
-use Module\Booking\Shared\Domain\Booking\Repository\BookingRepositoryInterface;
+use Module\Booking\Shared\Domain\Booking\Service\BookingUnitOfWorkInterface;
 use Sdk\Booking\ValueObject\BookingId;
 use Sdk\Module\Contracts\UseCase\UseCaseInterface;
 
 class SendRequest implements UseCaseInterface
 {
     public function __construct(
-        private readonly BookingRepositoryInterface $bookingRepository,
+        private readonly BookingUnitOfWorkInterface $bookingUnitOfWork,
         private readonly RequestFactory $requestFactory,
         private readonly RequestingRules $requestRules,
-    ) {
-    }
+    ) {}
 
     public function execute(int $id): void
     {
-        $booking = $this->bookingRepository->findOrFail(new BookingId($id));
+        $booking = $this->bookingUnitOfWork->findOrFail(new BookingId($id));
 
         $this->requestRules->booking($booking);
 
-        $this->requestFactory->generate($booking, $this->requestRules->getRequestType());
+        $this->bookingUnitOfWork->commiting(function () use ($booking) {
+            $this->requestFactory->generate($booking, $this->requestRules->getRequestType());
+        });
+        $this->bookingUnitOfWork->touch($booking->id());
+        $this->bookingUnitOfWork->commit();
     }
 }
--- src/modules/Booking/Shared/Domain/Booking/Listener/UpdateHotelQuotaListener.php
+++ src/modules/Booking/Shared/Domain/Booking/Listener/UpdateHotelQuotaListener.php
@@ -5,6 +5,7 @@ namespace Module\Booking\Shared\Domain\Booking\Listener;
 use Module\Booking\Moderation\Application\Exception\HotelDetailsExpectedException;
 use Module\Booking\Moderation\Domain\Booking\Service\HotelBooking\QuotaManager\QuotaProcessingMethodFactory;
 use Module\Booking\Shared\Domain\Booking\Event\HotelBooking\QuotaChangedEventInterface;
+use Module\Booking\Shared\Domain\Booking\Repository\BookingRepositoryInterface;
 use Module\Booking\Shared\Domain\Booking\Repository\DetailsRepositoryInterface;
 use Sdk\Booking\Entity\Details\HotelBooking;
 use Sdk\Module\Contracts\Event\DomainEventInterface;
@@ -13,16 +14,16 @@ use Sdk\Module\Contracts\Event\DomainEventListenerInterface;
 class UpdateHotelQuotaListener implements DomainEventListenerInterface
 {
     public function __construct(
+        private readonly BookingRepositoryInterface $bookingRepository,
         private readonly DetailsRepositoryInterface $detailsRepository,
         private readonly QuotaProcessingMethodFactory $quotaProcessingMethodFactory
-    ) {
-    }
+    ) {}
 
     public function handle(DomainEventInterface $event): void
     {
         assert($event instanceof QuotaChangedEventInterface);
 
-        $booking = $event->booking();
+        $booking = $this->bookingRepository->findOrFail($event->bookingId());
         $details = $this->detailsRepository->findOrFail($booking->id());
         if (!$details instanceof HotelBooking) {
             throw new HotelDetailsExpectedException();
