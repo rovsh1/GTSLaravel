f70cd98ccb10227d8dbaba695f9310376a47f5e6
Гурьянов Сергей <s16121986@yandex.ru>
2023-12-15 13:54:46 +0300

    feat: add horizon to menu

--- apps/admin/app/View/Components/Icon.php
+++ apps/admin/app/View/Components/Icon.php
@@ -18,6 +18,7 @@ class Icon extends Component
         'reports' => 'bar_chart',
         'booking' => 'airplane_ticket',
         'site' => 'language',
+        'horizon' => 'layers',
     ];
 
     protected readonly ?string $key;
