48c103aff8462b11f127e5cb4c6444a5c0d8536d
Dmitrii Artemov <dimochka1994@yandex.ru>
2023-12-27 10:38:16 +0300

    fix: images path

--- apps/hotel/routes/image.php
+++ apps/hotel/routes/image.php
@@ -4,7 +4,7 @@ use App\Hotel\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(ImageController::class)
-    ->prefix('img')
+    ->prefix('images')
     ->as('images.')
     ->group(function () {
         Route::get('/', 'index')->name('index');
