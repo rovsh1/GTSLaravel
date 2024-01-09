b523cda3cab5dc7b9b3fa15351baa427ef927de4
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-01-09 22:26:50 +0300

    fix: invoice pdf new template (GTS-2352)

--- resources/pdf-templates/voucher/_partials/guests.blade.php
+++ resources/pdf-templates/voucher/_partials/guests.blade.php
@@ -1,8 +1,8 @@
 <div class="service-guests">
     <p>{{ __('Гости (:count)', ['count' => count($service->guests)]) }}:</p>
     <ol>
-        @foreach($service->guests ?? [] as $index => $guest)
-            <li>{{++$index}}. {{ $guest->fullName }} ({{ $guest->countryName }})</li>
+        @foreach($service->guests ?? [] as $guest)
+            <li>{{ $guest->fullName }} ({{ $guest->countryName }})</li>
         @endforeach
     </ol>
 </div>
