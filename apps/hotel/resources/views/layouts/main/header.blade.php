f053cf791e62604725e45530d4e5b6b64cde63f3
Dmitry Huk <gukdmitry.s@gmail.com>
2024-01-12 23:09:22 +0200

    feat: removing .env files from front apps (GTS-2368)

--- apps/hotel/resources/views/layouts/main/header.blade.php
+++ apps/hotel/resources/views/layouts/main/header.blade.php
@@ -1,10 +1,5 @@
 <header>
     <div class="logo-wrapper">
-        <button id="btn-sitemap" aria-label="Open sidebar menu">
-            <svg width="100%" height="100%" viewBox="0 0 24 24" fill="currentColor" preserveAspectRatio="xMidYMid meet" focusable="false">
-                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-            </svg>
-        </button>
         <a class="logo" href="/" aria-label="Go to home page"></a>
     </div>
 
