34492e3c7080d5e179d32db56057eea1f4298b05
Dmitry Huk <gukdmitry.s@gmail.com>
2024-01-18 22:13:23 +0200

    feat: TL badge for booking show (GTS-2408)

---
+++ apps/admin/resources/views/booking/shared/components/BookingPanel/components/BookingTravelineFlag.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import { computed } from 'vue'
+
+import { useBookingStore } from '~resources/views/booking/shared/store/booking'
+
+const bookingStore = useBookingStore()
+const isTravelineIntegrationEnabled = computed(() => bookingStore.booking?.details?.isTravelineIntegrationEnabled)
+
+</script>
+
+<template>
+  <div v-if="bookingStore.isHotelBooking && isTravelineIntegrationEnabled">
+    <span title="Интеграция с Traveline" class="traveline-badge large">TL</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.traveline-badge.large {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+  height: 32px;
+}
+</style>
