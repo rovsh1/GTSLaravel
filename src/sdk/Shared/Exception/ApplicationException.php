88d2482f868067d2242c77ec2c20e00c6bfc900b
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-01-31 13:14:46 +0300

    fix: booking period validation (GTS-2455)

--- src/sdk/Shared/Exception/ApplicationException.php
+++ src/sdk/Shared/Exception/ApplicationException.php
@@ -30,6 +30,7 @@ abstract class ApplicationException extends \RuntimeException
     public const ORDER_WITHOUT_BOOKINGS                      = 1019;
     public const BOOKING_CAR_BID_TOO_MANY_GUESTS             = 1020;
     public const BOOKING_SUPPLIER_PENALTY_CANNOT_BE_ZERO     = 1021;
+    public const BOOKING_PERIOD_DATES_CANNOT_BE_EQUAL        = 1022;
 
     private static array $errorsArray;
 
