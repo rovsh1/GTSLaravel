85b0ce0060669d90cf640d9304e9103112afee76
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-02-19 15:34:31 +0300

    fix: Remove empty parts

--- apps/admin/factories/booking/status-settings.php
+++ apps/admin/factories/booking/status-settings.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Admin\Components\Factory\Factory;
-
-return Factory::key('status-settings')
-    ->category(Factory::CATEGORY_BOOKING)
-    ->group(Factory::GROUP_SETTINGS)
-//    ->controller(\App\Admin\Http\Controllers\Booking\StatusSettingsController::class)
-    ->titles([
-        "index" => "Настройка статусов"
-    ]);
+//
+//return Factory::key('status-settings')
+//    ->category(Factory::CATEGORY_BOOKING)
+//    ->group(Factory::GROUP_SETTINGS)
+////    ->controller(\App\Admin\Http\Controllers\Booking\StatusSettingsController::class)
+//    ->titles([
+//        "index" => "Настройка статусов"
+//    ]);
