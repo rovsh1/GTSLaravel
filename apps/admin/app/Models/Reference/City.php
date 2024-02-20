34b3039a4f48cfd03b04465c4142a1badaa9456a
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-02-20 09:37:08 +0300

    fix: city filters error (GTS-2515)

--- apps/admin/app/Models/Reference/City.php
+++ apps/admin/app/Models/Reference/City.php
@@ -38,7 +38,7 @@ class City extends Model
 
     public $timestamps = false;
 
-    protected array $quicksearch = ['id', 'r_cities.name%'];
+    protected array $quicksearch = ['id', 'r_cities_translation.name%'];
 
     protected array $translatable = ['name'];
 
