11bcfa035c4e0fa7d63752c546766e91660a45fc
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-02-28 16:50:06 +0300

    fix: use DbContext instead BookingRepository for log request changes

--- packages/core/booking/requesting/src/Domain/Listener/RegisterChangesListener.php
+++ packages/core/booking/requesting/src/Domain/Listener/RegisterChangesListener.php
@@ -2,7 +2,6 @@
 
 namespace Pkg\Booking\Requesting\Domain\Listener;
 
-use Illuminate\Support\Facades\Log;
 use Module\Booking\Shared\Domain\Booking\DbContext\BookingDbContextInterface;
 use Pkg\Booking\Requesting\Domain\Service\ChangesRegistrar\RegistrarFactory;
 use Sdk\Booking\Enum\StatusEnum;
@@ -14,35 +13,26 @@ use Sdk\Shared\Event\IntegrationEventMessage;
 class RegisterChangesListener implements IntegrationEventListenerInterface
 {
     public function __construct(
-        private readonly BookingDbContextInterface $bookingRepository,
+        private readonly BookingDbContextInterface $bookingDbContext,
         private readonly RegistrarFactory $registrarFactory,
     ) {}
 
     public function handle(IntegrationEventMessage $message): void
     {
-        Log::debug('RegisterChangesListener::handle start', ['message' => $message]);
         $event = $message->event;
         assert($event instanceof BookingEventInterface);
 
-        Log::debug('RegisterChangesListener::handle event', ['event' => $event]);
-        $booking = $this->bookingRepository->findOrFail(new BookingId($event->bookingId));
+        $booking = $this->bookingDbContext->findOrFail(new BookingId($event->bookingId));
         if (!$this->needStoreChanges($booking->status()->value())) {
-            Log::debug('RegisterChangesListener::handle dont need store', ['status' => $booking->status()->value()]);
-
             return;
         }
 
-        Log::debug('RegisterChangesListener::handle need store');
         $registrar = $this->registrarFactory->build($event);
         if (!$registrar) {
-            Log::debug('RegisterChangesListener::handle dont find registrar');
-
             return;
         }
 
-        Log::debug('RegisterChangesListener::handle register');
         $registrar->register($event);
-        Log::debug('RegisterChangesListener::handle end');
     }
 
     private function needStoreChanges(StatusEnum $status): bool
