b047c8fc2b6fd3487641e6ad42a941c05e7cb480
Dmitry Huk <gukdmitry.s@gmail.com>
2024-03-01 15:45:24 +0200

    feat: building vue components

---
+++ apps/shared/libs/components/src/Bootstrap/BootstrapCard/components/BootstrapCardTitle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="card-title">
+    <h2>{{ title }}</h2>
+  </div>
+</template>
---
+++ apps/shared/libs/components/src/ButtonLoadingSpinner.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+import { BootstrapSeverity } from '~components/Bootstrap/lib'
+import LoadingSpinner from '~components/LoadingSpinner.vue'
+
+type SpinnerSeverity = BootstrapSeverity
+
+type Severity = SpinnerSeverity
+withDefaults(defineProps<{
+  severity?: Severity
+  show: boolean
+}>(), {
+  severity: 'light',
+  show: false,
+})
+
+</script>
+
+<template>
+  <LoadingSpinner v-if="show" :severity="severity" class="button-spinner" />
+</template>
+
+<style scoped lang="scss">
+.button-spinner {
+  width: 11px;
+  height: 12px;
+  margin-left: 0.125rem;
+}
+</style>
