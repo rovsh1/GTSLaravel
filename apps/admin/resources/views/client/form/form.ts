7554389399c81e7ba6514103f8bb3631bd04ee6e
Dmitry Huk <gukdmitry.s@gmail.com>
2024-03-04 14:25:59 +0200

    feat: order fields for create client from (GTS-2518)

--- apps/admin/resources/views/client/form/form.ts
+++ apps/admin/resources/views/client/form/form.ts
@@ -1,11 +1,17 @@
+import { useSelectElement } from 'gts-common/widgets/select-element'
+
 import '~resources/views/main'
 
 $(() => {
   const $legalTypeSelect = $('#form_data_type')
   const $genderInput = $('#form_data_gender')
+  const $countryField = $('div.field-country_id')
+  const $countrySelect = $countryField.find('#form_data_country_id')
+  const $cityField = $('div.field-city_id')
+  const $citySelect = $cityField.find('#form_data_city_id')
   let $genderSelect: JQuery<HTMLSelectElement>
 
-  const initGenderSelect = (): void => {
+  const initGenderSelect = async () => {
     if ($genderSelect !== undefined) {
       return
     }
@@ -17,27 +23,40 @@ $(() => {
       .append('<option value="2">Женский</option>')
 
     $genderInput.after($genderSelect)
+    await useSelectElement($genderSelect[0])
     $genderSelect.change(() => {
       $genderInput.val($genderSelect?.val() || '')
     })
   }
 
-  const toggleGenderField = (): void => {
+  const toggleFieldsByClientType = async () => {
     const $genderField = $('div.field-gender')
     if ($legalTypeSelect.val() !== '1') {
       $genderField.hide().toggleClass('field-required', false)
       $genderInput.removeAttr('required')
       $genderSelect?.removeAttr('required')
+
+      $citySelect.val('').change()
+      $cityField.show().toggleClass('field-required', true)
+      $citySelect.attr('required', 'required')
+      $countryField.hide().toggleClass('field-required', false)
+      $countrySelect.removeAttr('required')
       return
     }
-    initGenderSelect()
-    $genderSelect.val('')
+    await initGenderSelect()
+    $genderSelect.val('').change()
     $genderField.show().toggleClass('field-required', true)
     $genderSelect.attr('required', 'required')
     $genderInput.attr('required', 'required')
+
+    $countrySelect.val('').change()
+    $cityField.hide().toggleClass('field-required', false)
+    $citySelect.removeAttr('required')
+    $countryField.show().toggleClass('field-required', true)
+    $countrySelect.attr('required', 'required')
   }
 
   $legalTypeSelect
-    .ready(toggleGenderField)
-    .change(toggleGenderField)
+    .ready(toggleFieldsByClientType)
+    .change(toggleFieldsByClientType)
 })
