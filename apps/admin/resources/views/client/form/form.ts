b5093be7a3d308530938bc9516072fff690a1834
Dmitry Huk <gukdmitry.s@gmail.com>
2024-03-04 16:51:18 +0200

    fix: create client form

--- apps/admin/resources/views/client/form/form.ts
+++ apps/admin/resources/views/client/form/form.ts
@@ -10,6 +10,7 @@ const { id } = requestInitialData(z.object({
 
 $(() => {
   const isEditMode = id !== null
+  let isFirstInit = true
   const $legalTypeSelect = $('#form_data_type')
   const $genderInput = $('#form_data_gender')
   const $countryField = $('div.field-country_id')
@@ -17,14 +18,12 @@ $(() => {
   const $cityField = $('div.field-city_id')
   const $citySelect = $cityField.find('#form_data_city_id')
   let $genderSelect: JQuery<HTMLSelectElement>
-
-  console.log({ isEditMode })
+  const editedLegalType = $legalTypeSelect?.val() || null
 
   const initGenderSelect = async () => {
     if ($genderSelect !== undefined) {
       return
     }
-
     // @ts-expect-error тут все ок
     $genderSelect = $('<select />', { class: 'form-select form-control' })
       .append('<option disabled selected></option>')
@@ -33,6 +32,10 @@ $(() => {
 
     $genderInput.after($genderSelect)
     await useSelectElement($genderSelect[0])
+    const preGenderValue = $genderInput.val()
+    if (preGenderValue) {
+      $genderSelect.val(preGenderValue).change()
+    }
     $genderSelect.change(() => {
       $genderInput.val($genderSelect?.val() || '')
     })
@@ -40,29 +43,37 @@ $(() => {
 
   const toggleFieldsByClientType = async () => {
     const $genderField = $('div.field-gender')
-    if ($legalTypeSelect.val() !== '1') {
-      $genderField.hide().toggleClass('field-required', false)
-      $genderInput.removeAttr('required')
-      $genderSelect?.removeAttr('required')
-
-      $citySelect.val('').change()
-      $cityField.show().toggleClass('field-required', true)
-      $citySelect.attr('required', 'required')
-      $countryField.hide().toggleClass('field-required', false)
-      $countrySelect.removeAttr('required')
-      return
-    }
-    await initGenderSelect()
-    $genderSelect.val('').change()
-    $genderField.show().toggleClass('field-required', true)
-    $genderSelect.attr('required', 'required')
-    $genderInput.attr('required', 'required')
-
-    $countrySelect.val('').change()
+    $genderField.hide().toggleClass('field-required', false)
+    $genderField.hide().toggleClass('field-required', false)
+    $genderInput.removeAttr('required')
+    $genderSelect?.removeAttr('required')
+    $countryField.hide().toggleClass('field-required', false)
+    $countrySelect.removeAttr('required')
     $cityField.hide().toggleClass('field-required', false)
     $citySelect.removeAttr('required')
-    $countryField.show().toggleClass('field-required', true)
-    $countrySelect.attr('required', 'required')
+
+    if ($legalTypeSelect.val() === '2') {
+      $cityField.show().toggleClass('field-required', true)
+      $citySelect.attr('required', 'required')
+      $genderSelect.val('').change()
+      $countrySelect.val('').change()
+      if (!isFirstInit) {
+        $citySelect.val('').change()
+      }
+    } else if ($legalTypeSelect.val() === '1' && (!isEditMode || (isEditMode && editedLegalType !== $legalTypeSelect.val()))) {
+      await initGenderSelect()
+      $genderField.show().toggleClass('field-required', true)
+      $genderSelect.attr('required', 'required')
+      $genderInput.attr('required', 'required')
+      $countryField.show().toggleClass('field-required', true)
+      $countrySelect.attr('required', 'required')
+      $citySelect.val('').change()
+      if (!isFirstInit) {
+        $genderSelect.val('').change()
+        $countrySelect.val('').change()
+      }
+    }
+    isFirstInit = false
   }
 
   $legalTypeSelect
