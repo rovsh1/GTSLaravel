1bd8a0b580dbe224a2fd2b3cad63b9eba0649dc8
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-03-07 19:42:36 +0300

    fix: fix markup search (GTS-2562)

--- apps/admin/app/Models/Pricing/MarkupGroup.php
+++ apps/admin/app/Models/Pricing/MarkupGroup.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace App\Admin\Models\Pricing;
 
-use Illuminate\Database\Eloquent\Builder;
+use Sdk\Module\Database\Eloquent\HasQuicksearch;
 use Sdk\Module\Database\Eloquent\Model;
 use Sdk\Shared\Enum\Pricing\ValueTypeEnum;
 
 class MarkupGroup extends Model
 {
+    use HasQuicksearch;
+
     protected $table = 'client_markup_groups';
+
+    protected array $quicksearch = ['id', 'client_markup_groups.%name%'];
 
     protected $fillable = [
         'id',
@@ -22,16 +26,6 @@ class MarkupGroup extends Model
     protected $casts = [
         'type' => ValueTypeEnum::class
     ];
-
-    protected static function booted()
-    {
-        static::addGlobalScope('default', function (Builder $builder) {
-//            $builder
-//                ->addSelect('booking_hotel_accommodations.*')
-//                ->join('bookings', 'bookings.id', '=', 'booking_hotel_accommodations.booking_id')
-//                ->addSelect('bookings.order_id as booking_order_id');
-        });
-    }
 
     public function __toString()
     {
