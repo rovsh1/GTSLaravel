03bfe7e1c4026d1ec4e9e9ef44174339b886ac5f
Dmitry Huk <gukdmitry.s@gmail.com>
2024-03-10 16:36:42 +0200

    feat: edit room descriptions ho (GTS-2551)

--- apps/hotel/resources/views/room-form/room-form.ts
+++ apps/hotel/resources/views/room-form/room-form.ts
@@ -3,5 +3,11 @@ import HtmlEditor from '~resources/js/plugins/htmleditor/htmleditor'
 import '~resources/views/main'
 
 $(async () => {
-  (new HtmlEditor('#room-text-textarea', {})).init()
+  const langs = ['ru', 'uz', 'en']
+
+  const initHtmlEditor = async (lang: string) => (new HtmlEditor(`#room-text-textarea-${lang}`, {})).init()
+  /* eslint-disable no-restricted-syntax, no-await-in-loop */
+  for (const lang of langs) {
+    await initHtmlEditor(lang)
+  }
 })
--- apps/hotel/resources/views/rooms/rooms.ts
+++ apps/hotel/resources/views/rooms/rooms.ts
@@ -1,6 +1,16 @@
 import '~resources/views/main'
 
 $(() => {
+  $('.usability-wrapper').each(function () {
+    const usabilityCheckerHeight = $(this).find('.usability-height-checker').height()
+    const usabilityHeight = $(this).find('.usability').height()
+    const expandButton = $(this).find('.usability-expand')
+    if (usabilityCheckerHeight !== undefined && usabilityHeight !== undefined && usabilityCheckerHeight <= usabilityHeight) {
+      $(this).find('.usability').height(0)
+      expandButton.remove()
+    }
+  })
+
   $('.usability-expand i').click((element) => {
     $(element.target).parent().parent().toggleClass('expand')
     const existExpandedElement = !!$('.usability-wrapper.expand').length
