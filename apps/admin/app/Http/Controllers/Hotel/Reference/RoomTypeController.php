94b375e4a66d7962fcf0e61af6d05e72dad84808
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-03-11 12:55:03 +0300

    feat: add room types list API

--- apps/admin/app/Http/Controllers/Hotel/Reference/RoomTypeController.php
+++ apps/admin/app/Http/Controllers/Hotel/Reference/RoomTypeController.php
@@ -3,11 +3,19 @@
 namespace App\Admin\Http\Controllers\Hotel\Reference;
 
 use App\Admin\Support\Http\Controllers\AbstractEnumController;
+use Illuminate\Http\JsonResponse;
 
 class RoomTypeController extends AbstractEnumController
 {
     protected function getPrototypeKey(): string
     {
         return 'hotel-room-type';
+    }
+
+    public function list(): JsonResponse
+    {
+        return response()->json(
+            $this->model->all()
+        );
     }
 }
