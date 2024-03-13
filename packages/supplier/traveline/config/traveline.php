c038949111eeafd8b120ce1591c801fdc570fcd4
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-03-13 17:28:21 +0300

    fix: traveline period timezone (GTS-2546)

--- packages/supplier/traveline/config/traveline.php
+++ packages/supplier/traveline/config/traveline.php
@@ -9,6 +9,6 @@ return [
     'notifications_url' => env('TRAVELINE_NOTIFICATIONS_URL'),
     'is_prices_for_residents' => env('TRAVELINE_IS_PRICES_FOR_RESIDENTS', false),
     'supported_currencies' => explode(',', env('TRAVELINE_SUPPORTED_CURRENCIES', 'UZS')),
-    'timezone' => env('TRAVELINE_TIMEZONE', 'Europe/Moscow'),
+    'timezone' => env('TRAVELINE_TIMEZONE', 'Asia/Tashkent'),
 ];
 
