93159fc4c2963c9c3d66aa86fc9b6bbdfea6dfb7
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-03-13 19:15:26 +0300

    fix: service booking report (GTS-2586)

--- packages/core/booking/reporting/src/Service/Factory/ServiceBookingDataFactory.php
+++ packages/core/booking/reporting/src/Service/Factory/ServiceBookingDataFactory.php
@@ -11,6 +11,7 @@ use Carbon\CarbonPeriod;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Builder as Query;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Sdk\Booking\Enum\StatusEnum;
 use Sdk\Shared\Contracts\Service\TranslatorInterface;
 use Sdk\Shared\Enum\ServiceTypeEnum;
@@ -19,8 +20,7 @@ class ServiceBookingDataFactory
 {
     public function __construct(
         private readonly TranslatorInterface $translator,
-    ) {
-    }
+    ) {}
 
     public function build(
         CarbonPeriod $endPeriod,
@@ -71,7 +71,10 @@ class ServiceBookingDataFactory
                 }
             })
             ->where(function (Builder $builder) use ($startPeriod, $endPeriod) {
-                $startPeriodCondition = !empty($startPeriod) ? [$startPeriod->getStartDate(), $startPeriod->getEndDate()] : null;
+                $startPeriodCondition = !empty($startPeriod) ? [
+                    $startPeriod->getStartDate(),
+                    $startPeriod->getEndDate()
+                ] : null;
                 $endPeriodCondition = [$endPeriod->getStartDate(), $endPeriod->getEndDate()];
 
                 $builder->where(function (Builder $query) use ($startPeriodCondition, $endPeriodCondition) {
@@ -108,22 +111,35 @@ class ServiceBookingDataFactory
         $bookingIds = $bookingsData->pluck('id')->toArray();
         $guestsIndexedByBookingId = [];
         if (count($bookingIds) > 0) {
-            $guestsIndexedByBookingId = Guest::query()
-                ->selectRaw('COALESCE(booking_car_bids.booking_id, booking_airport_guests.booking_id) as booking_id')
-                ->leftJoin('booking_airport_guests', 'booking_airport_guests.guest_id', 'order_guests.id')
-                ->leftJoin('booking_car_bid_guests', 'booking_car_bid_guests.guest_id', 'order_guests.id')
-                ->leftJoin('booking_car_bids', 'booking_car_bids.id', 'booking_car_bid_guests.car_bid_id')
-                ->where(function (Builder $builder) use ($bookingIds) {
-                    $builder->whereIn('booking_airport_guests.booking_id', $bookingIds)
-                        ->orWhereIn('booking_car_bids.booking_id', $bookingIds);
-                })
-                ->get()
+            $carBidGuests = DB::table('booking_car_bid_guests')
+                ->select('guest_id')
+                ->addSelect('booking_car_bids.booking_id')
+                ->join('booking_car_bids', 'booking_car_bids.id', 'booking_car_bid_guests.car_bid_id')
+                ->whereColumn('booking_car_bids.id', 'booking_car_bid_guests.car_bid_id')
+                ->whereIn('booking_car_bids.booking_id', $bookingIds)
+                ->get();
+
+            $airportBookingGuests = DB::table('booking_airport_guests')
+                ->whereIn('booking_airport_guests.booking_id', $bookingIds)
+                ->get();
+
+            $bookingGuests = $carBidGuests->merge($airportBookingGuests);
+            $guestIds = $bookingGuests->pluck('guest_id')->unique()->toArray();
+            $guests = Guest::query()->whereIn('order_guests.id', $guestIds)->get()->keyBy('id');
+            $guestsIndexedByBookingId = $bookingGuests
                 ->groupBy('booking_id')
                 ->map(
-                    fn(Collection $guests) => $guests->map(
-                        fn(Guest $guest) => "{$guest->name} ({$guest->country_name})"
-                    )->all()
-                );
+                    fn(Collection $guestIds) => $guestIds->map(
+                        function (\stdClass $guestId) use ($guests) {
+                            $guest = $guests[$guestId->guest_id] ?? null;
+                            if ($guest === null) {
+                                return null;
+                            }
+
+                            return "{$guest->name} ({$guest->country_name})";
+                        }
+                    )->filter()->all()
+                )->all();
         }
 
         $services = [];
@@ -141,7 +157,7 @@ class ServiceBookingDataFactory
             'serviceNames' => $services,
             'supplierNames' => $suppliers,
             'guestsIndexedByBookingId' => $guestsIndexedByBookingId,
-            'bookings' => $bookingsData,
+            'bookings' => $bookingsData->toArray(),
         ];
     }
 }
