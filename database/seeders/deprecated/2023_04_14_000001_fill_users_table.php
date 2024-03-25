6a91f027710b517a979427736042a5f5c76c74bb
Dmitrii Artemov <dimochka1994@yandex.ru>
2024-03-25 13:02:23 +0300

    fix: seeders

--- database/seeders/deprecated/2023_04_14_000001_fill_users_table.php
+++ database/seeders/deprecated/2023_04_14_000001_fill_users_table.php
@@ -6,11 +6,14 @@ use Illuminate\Support\Facades\DB;
 return new class extends Migration {
     public function up(): void
     {
+        $newDbName = DB::getDatabaseName();
+        $oldDbName = DB::connection('mysql_old')->getDatabaseName();
         DB::unprepared(
             'INSERT INTO users (id, client_id, country_id,presentation, gender, login, password,email,phone,post_id,address,note,status,role,birthday,created_at,updated_at)'
             . ' SELECT id, client_id, country_id,presentation, gender, login, password,email,phone,post_id,address,note,status,role,birthday,NOW(),NOW()'
-            . ' FROM ' . DB::connection('mysql_old')->getDatabaseName() . '.users'.
-            ' WHERE presentation NOT LIKE "%http%" AND presentation NOT LIKE "%oпрoс%" AND presentation NOT LIKE "%зapабатывaть%"'
+            . ' FROM ' . $oldDbName . '.users'.
+            ' WHERE presentation NOT LIKE "%http%" AND presentation NOT LIKE "%oпрoс%" AND presentation NOT LIKE "%зapабатывaть%"'.
+            " AND EXISTS (SELECT 1 FROM $newDbName.clients WHERE $newDbName.clients.id = $oldDbName.users.client_id)"
         );
     }
 
